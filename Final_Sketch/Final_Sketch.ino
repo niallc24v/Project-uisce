f93461f37baec29e69c3552652f8ac2c93acde3c
SamuelCroft <s00182360@e0033-08.campus.itsligo.ie>
2018-03-15 17:03:13 +0000

    Added other test sketches and began final sketch

--- Final_Sketch/Final_Sketch.ino
+++ Final_Sketch/Final_Sketch.ino
@@ -1,14 +1,11 @@
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-
+   Serial.begin(9600); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int sensorValue = analogRead(A3);
+    int sensorValue = analogRead(A3);
   Serial.println(sensorValue);
   delay(1000);
 }
-
-
